56d89f437ade3ba81cd29a3a809eb630eeb7970e
Khaled Alaa <khaled28ala@gmail.com>
2024-01-21 06:38:13 +0200

    make changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,7 @@
-User.destroy_all
-Post.destroy_all
-Comment.destroy_all
-Like.destroy_all
-
-users = []
-30.times do
-  users << User.create(
-    email: Faker::Internet.email(name: Faker::Name.name),
-    name: Faker::Name.name,
-    bio: Faker::Quote.most_interesting_man_in_the_world,
-    photo: "https://unsplash.com/photos/abcdef",
-    password: "123456"
-  )
-end
-
-# users.each do |user|
-#   rand(1..5).times do
-#     post = user.posts.create(
-#       title: Faker::Book.title,
-#       body: Faker::Quote.famous_last_words,
-#     )
-
-#     rand(0..5).times do
-#       post.comments.create(
-#         text: Faker::Lorem.sentence,
-#         author: users.sample
-#       )
-#     end
-
-#     post.update(likes_counter: rand(0..100))
-#   end
-# end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
